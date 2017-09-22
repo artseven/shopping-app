59dabfa86e472b1c46f245ae8e4292bc145405dc
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-09-22 16:24:09 -0400

    submitting form to array and displaying it

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,4 +1,5 @@
 import { Ingredient } from './../../models/ingredient';
+
 import { ShoppingListService } from './../../services/shopping-list.service';
 import { NgForm } from '@angular/forms/src/directives';
 import { Component } from '@angular/core';
@@ -10,14 +11,26 @@ import { Component } from '@angular/core';
   templateUrl: 'shopping-list.html',
 })
 export class ShoppingListPage {
+  listItems: Ingredient[];
   //injecting service to get access to its functions
   constructor(
       private slService: ShoppingListService
      ) {}
 
+  ionViewWillEnter() {
+    this.loadItems();
+  }
+
   onAddItem(form: NgForm) {
     // passing NgForm and it's values into function
     this.slService.addItem(form.value.ingredientName, form.value.amount);
     form.reset();
+    this.loadItems()
   }
+
+  private loadItems() {
+    this.listItems = this.slService.getItems();
+  }
+
+
 }
