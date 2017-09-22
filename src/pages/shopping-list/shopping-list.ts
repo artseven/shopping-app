4a91904de986e2c5d067e6b302dd30567d403f3d
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-09-22 16:43:59 -0400

    deleting ingredients

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -28,6 +28,11 @@ export class ShoppingListPage {
     this.loadItems()
   }
 
+  onDeleteItem(index: number) {
+    this.slService.removeItem(index);
+    this.loadItems();
+  }
+
   private loadItems() {
     this.listItems = this.slService.getItems();
   }
