59fd61a23a822b780e2cb936a213dd932532875e
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-09-22 15:22:33 -0400

    service finished

---
+++ src/services/shopping-list.service.ts
@@ -0,0 +1,25 @@
+import { Ingredient } from '../models/ingredient';
+
+export class ShoppingListService {
+    private ingredients: Ingredient[] = [];
+
+
+    addItem(name: string, amount: number) {
+        this.ingredients.push(new Ingredient(name, amount));
+    }
+
+    addItems(items: Ingredient[]) {
+        // deconstruct this array into individual elements and push it to existing array
+        // ES6 operator ------ |
+        this.ingredients.push(...items);
+    }
+
+    getItems() {
+        // to create a copy of array| instead of referencing to original one
+        return this.ingredients.slice();
+    }
+
+    removeItem(index: number) {
+        this.ingredients.splice(index, 1);
+    }
+}
