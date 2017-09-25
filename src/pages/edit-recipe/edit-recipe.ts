590a62ffb82142069890986c0b62fc28719ed921
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-09-25 19:47:17 -0400

    adding alert as validator

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,5 +1,5 @@
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActionSheetController, NavParams } from 'ionic-angular';
+import { ActionSheetController, AlertController, NavParams } from 'ionic-angular';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -13,7 +13,8 @@ export class EditRecipePage implements OnInit {
   recipeForm: FormGroup; 
 
   constructor(private navParams: NavParams,
-   private actionSheetController: ActionSheetController) {}
+   private actionSheetController: ActionSheetController,
+   private alertController: AlertController) {}
 
   ngOnInit() {
     this.mode = this.navParams.get('mode');
@@ -31,21 +32,51 @@ export class EditRecipePage implements OnInit {
         {
           text: 'Add Ingredient',
           handler: () => {
-
+            
           }
         },
         {
           text: 'Remove all Ingredients',
           role: 'destructive',
           handler: () => {
-            
+
           }
 
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel'
         }
       ]
     });
   }
-
+  
+  private createNewIngredientAlert() {
+    const newIngredientAlert = this.alertController.create({
+      title: 'Add Ingredient',
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Add',
+          handler: data => {
+            //matches first input name
+            if (data.name.trim() === '' || data.name == null) {
+              console.log('WRONG!');
+            }
+          }
+        }
+      ]
+    })
+  }
   private initializeForm() {
     this.recipeForm = new FormGroup({
       'title': new FormControl(null, Validators.required),
