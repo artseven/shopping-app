d9c9a8b4a556ff1a8359b650ee3d4145925f8f7a
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-09-25 19:07:18 -0400

    typescript form created

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,3 +1,4 @@
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { NavParams } from 'ionic-angular';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,11 +9,20 @@ import { Component, OnInit } from '@angular/core';
 export class EditRecipePage implements OnInit {
   mode = 'New';
   selectOptions = ['Easy', 'Medium', 'Hard'];
+  recipeForm: FormGroup; 
 
   constructor(private navParams: NavParams) {}
 
   ngOnInit() {
     this.mode = this.navParams.get('mode');
+  }
+
+  private initializeForm() {
+    this.recipeForm = new FormGroup({
+      'title': new FormControl(null, Validators.required),
+      'description': new FormControl(null, Validators.required),
+      'difficulty': new FormControl('Medium', Validators.required)
+    });
   }
 
 }
