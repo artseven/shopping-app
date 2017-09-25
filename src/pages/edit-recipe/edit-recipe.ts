b3876399751e260ad1305318ad44d4f9b2478422
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-09-25 19:42:11 -0400

    action sheet controller added

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,17 +1,19 @@
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { NavParams } from 'ionic-angular';
+import { ActionSheetController, NavParams } from 'ionic-angular';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'page-edit-recipe',
   templateUrl: 'edit-recipe.html',
 })
+
 export class EditRecipePage implements OnInit {
   mode = 'New';
   selectOptions = ['Easy', 'Medium', 'Hard'];
   recipeForm: FormGroup; 
 
-  constructor(private navParams: NavParams) {}
+  constructor(private navParams: NavParams,
+   private actionSheetController: ActionSheetController) {}
 
   ngOnInit() {
     this.mode = this.navParams.get('mode');
@@ -20,6 +22,28 @@ export class EditRecipePage implements OnInit {
 
   onSubmit() {
     console.log(this.recipeForm);
+  }
+
+  onManageIngredients() {
+    const actionSheet = this.actionSheetController.create({
+      title: 'What do you want to do?',
+      buttons: [
+        {
+          text: 'Add Ingredient',
+          handler: () => {
+
+          }
+        },
+        {
+          text: 'Remove all Ingredients',
+          role: 'destructive',
+          handler: () => {
+            
+          }
+
+        }
+      ]
+    });
   }
 
   private initializeForm() {
