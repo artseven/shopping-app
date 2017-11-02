9bef55e6089bb48f581715a3635043e3cead3ff4
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-11-02 13:10:31 -0400

    navigate to recipe is working

--- src/pages/recipes/recipes.ts
+++ src/pages/recipes/recipes.ts
@@ -1,3 +1,4 @@
+import { RecipePage } from '../recipe/recipe';
 import { RecipesService } from './../../services/recipes.service';
 import { Recipe } from './../../models/recipe';
 import { EditRecipePage } from './../edit-recipe/edit-recipe';
@@ -24,7 +25,8 @@ export class RecipesPage {
     this.navCtrl.push(EditRecipePage, {mode: 'New'});
   }
 
-  onLoadRecipe() {
+  onLoadRecipe(recipe:Recipe, index:number) {
+    this.navCtrl.push(RecipePage, {recipe: recipe, index: index});
 
   }
 
