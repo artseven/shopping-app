9099316d0773b8bbf86e39f66df34f80ef0c2fb3
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-11-03 11:13:09 -0400

    ViewChild nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
+import { SignupPage } from '../pages/signup/signup';
 import { SigninPage } from '../pages/signin/signin';
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { MenuController, NavController, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -12,14 +13,25 @@ import { TabsPage } from './../pages/tabs/tabs';
 export class MyApp {
   tabsPage:any = TabsPage;
   signinPage:any = SigninPage;
+  signupPage:any = SignupPage;
+  @ViewChild('nav') nav: NavController;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private menuCtrl: MenuController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  onLoad(page: any) {
+    this.nav.setRoot(page);
+    this.menuCtrl.close();
+  }
+
+  onLogout() {
+    
   }
 }
 
