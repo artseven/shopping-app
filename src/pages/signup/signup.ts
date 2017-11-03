d83da904e2932ef75aec759a6c75bfd4e6a16039
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-11-03 12:33:01 -0400

    response errors handled

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,7 +1,11 @@
 import { AuthService } from '../../services/auth';
 import { NgForm } from '@angular/forms/src/directives';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage,
+   LoadingController, 
+   NavController, 
+   NavParams,
+   AlertController } from 'ionic-angular';
 
 
 @IonicPage()
@@ -10,11 +14,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'signup.html',
 })
 export class SignupPage {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private loadingCtrl: LoadingController,
+    private alertCtrl: AlertController) {}
 
     onSignup(form: NgForm) {
+      const loading = this.loadingCtrl.create({
+        content: 'Signing you up...'
+      });
+      loading.present();
       this.authService.signup(form.value.email, form.value.password)
-        .then(data => console.log(data))
-        .catch(error => console.log(error));
+        .then(data => {
+          loading.dismiss();
+        })
+        .catch(error =>{
+          loading.dismiss();
+          const alert = this.alertCtrl.create({
+            title: 'Signup failed!',
+            message: error.message,
+            buttons: ['Ok']
+          });
+          alert.present();
+        });
     }
 }
