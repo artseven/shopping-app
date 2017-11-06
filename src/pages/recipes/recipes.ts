f0224a7ebd6f57eadde7f368cc812089dc4ba9ed
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-11-06 16:12:56 -0500

    implementing logic to fetch data

--- src/pages/recipes/recipes.ts
+++ src/pages/recipes/recipes.ts
@@ -1,8 +1,10 @@
+import { AuthService } from '../../services/auth';
+import { RecipesOptionsPage } from './recipes-options/recipes-options';
 import { RecipePage } from '../recipe/recipe';
 import { RecipesService } from './../../services/recipes.service';
 import { Recipe } from './../../models/recipe';
 import { EditRecipePage } from './../edit-recipe/edit-recipe';
-import { NavController } from 'ionic-angular';
+import { LoadingController, NavController, PopoverController } from 'ionic-angular';
 import { Component } from '@angular/core';
 
 @Component({
@@ -13,7 +15,11 @@ export class RecipesPage {
   recipes: Recipe[];
   constructor(
     private navCtrl: NavController,
-    private recipesService: RecipesService) {
+    private recipesService: RecipesService,
+    private loadingCtrl: LoadingController,
+    private popoverCtrl: PopoverController,
+    private authSrv: AuthService
+  ) {
   }
 
   ionViewWillEnter(){
@@ -28,6 +34,21 @@ export class RecipesPage {
   onLoadRecipe(recipe: Recipe, index: number) {
     this.navCtrl.push(RecipePage, {recipe: recipe, index: index});
 
+  }
+
+  onShowOptions(event: MouseEvent) {
+    const loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    })
+    const popover = this.popoverCtrl.create(RecipesOptionsPage);
+    popover.present({ev: event});
+    popover.onDidDismiss(
+      data=> {
+        if (data.action == 'load') {
+          loading.present();
+          this.authSrv
+        }
+      })
   }
 
 }
