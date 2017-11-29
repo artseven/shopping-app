010eb238f8f49012e5540dc90aea8ef7a860ccec
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-11-29 14:54:21 -0500

    adding store and fetch methods to recipes page

--- src/services/recipes.service.ts
+++ src/services/recipes.service.ts
@@ -1,33 +1,53 @@
+import { AuthService } from './auth';
+import { Http } from '@angular/http';
 import { Ingredient } from './../models/ingredient';
 import { Recipe } from './../models/recipe';
-
+import { Injectable } from '@angular/core';
+import 'rxjs/Rx';
+@Injectable()
 export class RecipesService {
-    private recipes: Recipe[] = [];
+  private recipes: Recipe[] = [];
 
-    addRecipe(title: string,
-              description: string,
-              difficulty: string, 
-              ingredients: Ingredient[]) {
-        this.recipes.push(new Recipe(title, description, difficulty, ingredients));
-        console.log(this.recipes);
-    }
+  constructor(
+    private http: Http,
+    private authSrv: AuthService
+  ) {}
 
-    getRecipes() {
-        return this.recipes.slice();
-    }
+  addRecipe(title: string,
+            description: string,
+            difficulty: string,
+            ingredients: Ingredient[]) {
+      this.recipes.push(new Recipe(title, description, difficulty, ingredients));
+      console.log(this.recipes);
+  }
 
-    updateRecipe(index: number,
-                 title: string,
-                 description: string,
-                 difficulty: string,
-                 ingredients: Ingredient[]) {
-        this.recipes[index] = new Recipe(title, description, difficulty, ingredients);
-    }
+  getRecipes() {
+      return this.recipes.slice();
+  }
+
+  updateRecipe(index: number,
+                title: string,
+                description: string,
+                difficulty: string,
+                ingredients: Ingredient[]) {
+      this.recipes[index] = new Recipe(title, description, difficulty, ingredients);
+  }
 
 
-    removeRecipe(index: number) {
-        this.recipes.splice(index, )
-    }
+  removeRecipe(index: number) {
+      this.recipes.splice(index, )
+  }
+
+  storeList(token: string) {
+    const userId = this.authSrv.getActiveUser().uid;
+    return this.http.put('https://ionic3-recipesbook.firebaseio.com/' + userId + '/recipes.json?auth=' + token, this.recipes)
+  }
+
+
+  fetchList(token: string) {
+    const userId = this.authSrv.getActiveUser().uid;
+    return this.http.get('https://ionic3-recipesbook.firebaseio.com/' + userId + '/recipes.json?auth=' + token)
+  }
 
 
 }
